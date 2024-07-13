995a423088c41d39d85b8e1b9b39d62969c0a0b8
Ujjwal Tiwari <imujjwal@gmail.com>
2024-07-14 01:48:36 +0530

    signup and signin

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Login = () => {
+  const [isSignUp, setSignUp]=useState(true);
+
+  function toggleHandler(){
+    setSignUp(!isSignUp);
+  }
+
   return (
     <div>
       <Header/>
@@ -11,14 +17,34 @@ const Login = () => {
       </div>
       <div className='absolute bg-opacity-70 bg-black mt-28 mx-auto right-0 left-0 w-3/12 h-4/6'>
         <form className='flex flex-col '>
-        <h1 className='p-1 m-1 text-3xl font-bold text-white float-left'>Sign Up</h1>
+        <h1 className='p-1 m-1 text-3xl font-bold text-white float-left'>{isSignUp ?"Sign In" :"Sign Up"}</h1>
+        {
+          !isSignUp&&(
+             <input type='text' placeholder='Name' className='p-2 w-4/5 m-2 ml-10 bg-opacity-80 bg-black text-white'></input>
+          )
+            
+           
+          }
           <input type='text' placeholder='email' className='p-2 w-4/5 m-2 ml-10 bg-opacity-80 bg-black text-white'></input>
-          <input type='text' placeholder='password' className='p-2 w-4/5 m-2 ml-10 bg-opacity-80 bg-black text-white'></input>
-          <button className='p-2 w-4/5 m-2 text-2xl font-bold text-white bg-red-700 ml-10'>Sign Up</button>
-          <h3 className='p-1 w-10 m-1 ml-40 font-bold text-white border border-black '>or</h3>
-          <h2 className='p-2 w-2/5 m-2 ml-28 font-bold text-white float-left'>Forget password?</h2>
+          <input type='password' placeholder='password' className='p-2 w-4/5 m-2 ml-10 bg-opacity-80 bg-black text-white'></input>
+          <button className='p-2 w-4/5 m-2 text-2xl font-bold text-white bg-red-700 ml-10'>{isSignUp ?"Sign In" :"Sign Up"}</button>
           
-          <h2 className='p-1 ml-10 text-zinc-400'>New to Netflix?<span className='p-1 m-1 text-lg font-bold text-white'>Sign up now</span></h2>
+          {
+            isSignUp&&(<>
+              <h3 className='p-1 w-10 m-1 ml-40 font-bold text-white'>or</h3>
+              <h2 className='p-1 w-2/5 m-1 ml-28 font-bold text-white float-left'>Forget password?</h2>
+            </>)
+          }
+          
+          {
+            isSignUp?(
+              <h2 className='p-1 ml-10 text-zinc-400'>New to Netflix?<span onClick={()=>{toggleHandler()}}  className='p-1 m-1 text-lg font-bold text-white cursor-pointer'>Sign up now</span></h2>
+            ):(<>
+              <h3 className='p-1 w-10 m-1 ml-40 font-bold text-white'>or</h3>
+              <h2 className='p-1 ml-10 mt-9 text-zinc-400'>Already SignUp?<span onClick={()=>{toggleHandler()}}  className='p-1 m-1 text-lg font-bold text-white cursor-pointer bg-zinc-700 rounded-lg'>Sign In</span></h2>
+            </>)
+          }
+          
         </form>
       </div>
     </div>
